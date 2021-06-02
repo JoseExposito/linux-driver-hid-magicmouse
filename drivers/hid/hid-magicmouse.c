8f71924b9a2ab4136863259bf14c1c537fc95c0c
José Expósito <jose.exposito89@gmail.com>
2021-06-02 08:53:32 +0200

    Add patch from the mailing list

--- drivers/hid/hid-magicmouse.c
+++ drivers/hid/hid-magicmouse.c
@@ -969,7 +969,7 @@ static int magicmouse_probe(struct hid_device *hdev,
 	    (id->product == USB_DEVICE_ID_APPLE_MAGICTRACKPAD2 ||
 	     id->product == USB_DEVICE_ID_APPLE_MAGICMOUSE2) &&
 	    hdev->type != HID_TYPE_USBMOUSE)
-		return 0;
+		return -ENODEV;
 
 	msc = devm_kzalloc(&hdev->dev, sizeof(*msc), GFP_KERNEL);
 	if (msc == NULL) {
